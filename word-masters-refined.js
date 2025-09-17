5080cf4f7cd9695d6b000a2dac935b27d645e2b8
Timur Jalilov <timurjalilov1@gmail.com>
2025-09-17 15:02:17 +0100

    test

--- word-masters-refined.js
+++ word-masters-refined.js
@@ -1,6 +1,6 @@
 const ANSWER_LENGTH = 5
 const NUMBER_OF_TRIES = 6
-const loadingIcon = document.querySelector(".loading-icon")
+const loadingIcon = document.querySelector('.loading-icon')
 
 //State of the function.
 let userLetters = document.querySelectorAll(`.container-1`)
@@ -11,7 +11,7 @@ let gameEnd = false
 
 function setLoading(isLoading) {
   //Toggles the class hidden on the loadingIcon html element.
-  loadingIcon.classList.toggle("hidden", !isLoading)
+  loadingIcon.classList.toggle('hidden', !isLoading)
 }
 
 function isLetter(letter) {
@@ -36,7 +36,7 @@ function makeMap(array) {
 
 async function getWordOfTheDay() {
   //Getting the word of the day.
-  const response = await fetch("https://words.dev-apis.com/word-of-the-day")
+  const response = await fetch('https://words.dev-apis.com/word-of-the-day')
   const processedResponse = await response.json()
   isLoading = false
   setLoading(isLoading)
@@ -48,8 +48,8 @@ async function validateWord(word) {
   isLoading = true
   setLoading(isLoading)
 
-  const response = await fetch("https://words.dev-apis.com/validate-word", {
-    method: "POST",
+  const response = await fetch('https://words.dev-apis.com/validate-word', {
+    method: 'POST',
     body: JSON.stringify({ word: word }),
   })
   const processedResponse = await response.json()
@@ -64,9 +64,9 @@ function handleInvalidWord() {
   //For each letter container it removes the class invalid-word if it's already there and 100 milliseconds later adds it.
   //setTimeout is used because to trigger the animation we have to add the class, but if we call add and remove back to back the animation wil not have enough time to show.
   userLetters.forEach((userLetter) => {
-    userLetter.classList.remove("invalid-word")
+    userLetter.classList.remove('invalid-word')
     setTimeout(() => {
-      userLetter.classList.add("invalid-word")
+      userLetter.classList.add('invalid-word')
     }, 100)
   })
 }
@@ -74,7 +74,7 @@ function handleInvalidWord() {
 async function initialise() {
   //Getting the word of the day and splitting it into a array of individual letters.
   const wordOfDay = await getWordOfTheDay()
-  const wordOfDayLetters = wordOfDay.split("")
+  const wordOfDayLetters = wordOfDay.split('')
 
   function handleLetter(letter) {
     //Handling adding the letter to innerHTML of th letter container and incrementing the index.
@@ -88,7 +88,7 @@ async function initialise() {
     //Handling the deletion of the letter from the HTML container and decrementing the letter index.
     if (userLetterIndex > 0) {
       userLetterIndex--
-      userLetters[userLetterIndex].innerHTML = ""
+      userLetters[userLetterIndex].innerHTML = ''
     }
   }
 
@@ -98,7 +98,7 @@ async function initialise() {
     //.join() function returns a comma separated string of the values in the array, if you give it a empty string it will remove the comma separation.
     const userWord = Array.from(userLetters)
       .map((userLetter) => userLetter.innerText)
-      .join("")
+      .join('')
 
     //If the user entered word is less than the 5 letters ignore the it.
     if (userWord.length < ANSWER_LENGTH) {
@@ -118,7 +118,7 @@ async function initialise() {
     //Goes through each letter in the answer and marks all that are fully correct.
     for (let i = 0; i < ANSWER_LENGTH; i++) {
       if (userLetters[i].innerText === wordOfDayLetters[i]) {
-        userLetters[i].classList.add("correct-letter")
+        userLetters[i].classList.add('correct-letter')
         wordOfDayMap[wordOfDayLetters[i]]--
       }
     }
@@ -130,17 +130,20 @@ async function initialise() {
         continue
       }
 
-      if (wordOfDayLetters.includes(userLetters[i].innerText) && wordOfDayMap[userLetters[i].innerText] > 0) {
-        userLetters[i].classList.add("close-letter")
+      if (
+        wordOfDayLetters.includes(userLetters[i].innerText) &&
+        wordOfDayMap[userLetters[i].innerText] > 0
+      ) {
+        userLetters[i].classList.add('close-letter')
         wordOfDayMap[userLetters[i].innerText]--
       } else {
-        userLetters[i].classList.add("wrong-letter")
+        userLetters[i].classList.add('wrong-letter')
       }
     }
 
     //If the user entered the correct word they win.
     if (userWord === wordOfDay) {
-      alert("You Win!")
+      alert('You Win!')
       gameEnd = true
     }
 
@@ -156,7 +159,7 @@ async function initialise() {
     userLetters = document.querySelectorAll(`.container-${rowNumber}`)
   }
 
-  document.addEventListener("keyup", function handleKeyPress(event) {
+  document.addEventListener('keyup', function handleKeyPress(event) {
     const action = event.key
 
     //If the game is finished they do nothing.
@@ -167,9 +170,9 @@ async function initialise() {
     //Listen to specific key presses and call the corresponding functions.
     if (isLetter(action)) {
       handleLetter(action)
-    } else if (action === "Backspace") {
+    } else if (action === 'Backspace') {
       handleDelete()
-    } else if (action === "Enter") {
+    } else if (action === 'Enter') {
       handleEnter()
     }
   })
